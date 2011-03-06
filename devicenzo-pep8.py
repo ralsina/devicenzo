27c59872d0b5f209990fd5f9feecd0561719d0de
roberto.alsina <roberto.alsina@1bbba601-83ea-880f-26a2-52609c2bd284>
2011-03-06 13:36:59 +0000

    Synced the PEP-8 compliant version. It's up to 127 SLOC now :-)

--- devicenzo-pep8.py
+++ devicenzo-pep8.py
@@ -13,17 +13,40 @@ class MainWindow(QtGui.QMainWindow):
         QtGui.QMainWindow.__init__(self)
         self.tabs = QtGui.QTabWidget(self, tabsClosable=True, movable=True,
             currentChanged=self.currentTabChanged,
-            elideMode=QtCore.Qt.ElideRight,
-            tabCloseRequested=lambda idx: self.tabs.widget(idx).deleteLater())
+            elideMode=QtCore.Qt.ElideRight, tabCloseRequested=lambda idx:
+                self.tabs.widget(idx).deleteLater())
         self.setCentralWidget(self.tabs)
         self.sb = self.statusBar()
         self.tabWidgets = []
+        self.star = QtGui.QAction(QtGui.QIcon.fromTheme("emblem-favorite"),
+            "Bookmark", self, checkable=True,
+            triggered=self.bookmarkPage, shortcut="Ctrl+d")
+        self.newtab = QtGui.QAction(QtGui.QIcon.fromTheme("document-new"),
+            "New Tab", self, triggered=lambda: self.addTab(),
+            shortcut="Ctrl+t")
+        self.bookmarks = self.get("bookmarks", {})
+        self.bookmarkPage()  # Load the bookmarks menu
+        self.history = self.get("history", []) + self.bookmarks.keys()
+        self.completer = QtGui.QCompleter(QtCore.QStringList([QtCore.QString(u)
+            for u in self.history]))
         self.addTab(url)
 
+    def close(self):
+        self.put("history", self.history)
+
+    def put(self, key, value):
+        "Persist an object somewhere under a given key"
+        settings.setValue(key, json.dumps(value))
+        settings.sync()
+
+    def get(self, key, default=None):
+        "Get the object 'key' in persistent storage, or the default value"
+        v = settings.value(key)
+        return json.loads(unicode(v.toString())) if v.isValid() else default
+
     def addTab(self, url=QtCore.QUrl()):
-        t = Tab(url, self)
-        self.tabs.addTab(t, "")
-        return t
+        self.tabs.setCurrentIndex(self.tabs.addTab(Tab(url, self), ""))
+        return self.tabs.currentWidget()
 
     def currentTabChanged(self, idx):
         wb = self.tabs.widget(idx)
@@ -37,49 +60,61 @@ class MainWindow(QtGui.QMainWindow):
         for w in self.tabWidgets[:-2]:
             w.show()
 
+    def bookmarkPage(self, v=None):
+        if v and v is not None:
+            self.bookmarks[unicode(self.tabs.currentWidget().url.text())] =\
+                unicode(self.tabs.currentWidget().title())
+        elif v is not None:
+            del (self.bookmarks[unicode(self.tabs.currentWidget().url.text())])
+        self.star.setMenu(QtGui.QMenu())
+        [self.star.menu().addAction(QtGui.QAction(title, self,
+            activated=lambda u=QtCore.QUrl(url):
+                self.tabs.currentWidget().load(u)))
+                for url, title in self.bookmarks.items()]
+        self.put('bookmarks', self.bookmarks)
+
+    def addToHistory(self, url):
+        self.history.append(url)
+        self.completer.setModel(QtGui.QStringListModel(
+            list(set(self.bookmarks.keys() + self.history))))
+
 
 class Tab(QtWebKit.QWebView):
     def __init__(self, url, container):
         self.container = container
         self.pbar = QtGui.QProgressBar()
-        QtWebKit.QWebView.__init__(self,
-            loadProgress=lambda v: (self.pbar.show(), self.pbar.setValue(v))
-                if self.amCurrent() else None,
-            loadFinished=self.pbar.hide, loadStarted=lambda: self.pbar.show()
-                if self.amCurrent() else None,
-            titleChanged=lambda t:
-                container.tabs.setTabText(container.tabs.indexOf(self), t) or
+        QtWebKit.QWebView.__init__(self, loadProgress=lambda v:
+            (self.pbar.show(), self.pbar.setValue(v))
+                if self.amCurrent() else None, loadFinished=self.pbar.hide,
+            loadStarted=lambda: self.pbar.show() if self.amCurrent() else None,
+            titleChanged=lambda t: container.tabs.setTabText(
+                container.tabs.indexOf(self), t) or
                 (container.setWindowTitle(t) if self.amCurrent() else None))
 
-        self.bookmarks = self.get("bookmarks", {})
         self.pbar.setMaximumWidth(120)
         container.sb.addPermanentWidget(self.pbar)
         self.pbar.hide()
 
         self.tb = QtGui.QToolBar("Main Toolbar")
-        for a in (QtWebKit.QWebPage.Back, QtWebKit.QWebPage.Forward,
-                  QtWebKit.QWebPage.Reload):
+        for a in (QtWebKit.QWebPage.Back,
+            QtWebKit.QWebPage.Forward, QtWebKit.QWebPage.Reload):
             self.tb.addAction(self.pageAction(a))
 
         self.url = QtGui.QLineEdit(returnPressed=lambda:
             self.setUrl(QtCore.QUrl.fromUserInput(self.url.text())))
+        self.url.setCompleter(container.completer)
         self.tb.addWidget(self.url)
-        self.star = QtGui.QAction(QtGui.QIcon.fromTheme("emblem-favorite"),
-            "Bookmark", self, checkable=True, triggered=self.bookmarkPage)
-        self.tb.addAction(self.star)
-        self.bookmarkPage()  # This triggers building the bookmarks menu
-        self.newtab = QtGui.QAction(QtGui.QIcon.fromTheme("document-new"),
-            "New Tab", self, triggered=self.createWindow)
-        self.tb.addAction(self.newtab)
+        self.tb.addAction(container.star)
+        self.tb.addAction(container.newtab)
 
+        # FIXME: if I was seriously golfing,
+        # all of these can go in a single lambda
         self.urlChanged.connect(lambda u: self.url.setText(u.toString()))
-        self.urlChanged.connect(lambda: self.url.setCompleter(
-            QtGui.QCompleter(QtCore.QStringList([
-                QtCore.QString(i.url().toString())
-                for i in self.history().items()]),
-            caseSensitivity=QtCore.Qt.CaseInsensitive)))
         self.urlChanged.connect(lambda u:
-            self.star.setChecked(unicode(u.toString()) in self.bookmarks))
+            container.addToHistory(unicode(u.toString())))
+        self.urlChanged.connect(lambda u:
+            container.star.setChecked(unicode(u.toString())
+            in container.bookmarks) if self.amCurrent() else None)
 
         self.statusBarMessage.connect(container.sb.showMessage)
         self.page().linkHovered.connect(lambda l:
@@ -88,18 +123,18 @@ class Tab(QtWebKit.QWebView):
         self.search = QtGui.QLineEdit(returnPressed=lambda:
             self.findText(self.search.text()))
         self.search.hide()
-        self.showSearch = QtGui.QShortcut("Ctrl+F", self,
-            activated=lambda: self.search.show() or self.search.setFocus())
-        self.hideSearch = QtGui.QShortcut("Esc", self,
-            activated=lambda: (self.search.hide(), self.setFocus()))
+        self.showSearch = QtGui.QShortcut("Ctrl+F", self, activated=lambda:
+            self.search.show() or self.search.setFocus())
+        self.hideSearch = QtGui.QShortcut("Esc", self, activated=lambda:
+            (self.search.hide(), self.setFocus()))
 
         self.quit = QtGui.QShortcut("Ctrl+Q", self, activated=self.close)
-        self.zoomIn = QtGui.QShortcut("Ctrl++", self,
-            activated=lambda: self.setZoomFactor(self.zoomFactor() + 0.2))
-        self.zoomOut = QtGui.QShortcut("Ctrl+-", self,
-            activated=lambda: self.setZoomFactor(self.zoomFactor() - 0.2))
-        self.zoomOne = QtGui.QShortcut("Ctrl+=", self,
-            activated=lambda: self.setZoomFactor(1))
+        self.zoomIn = QtGui.QShortcut("Ctrl++", self, activated=lambda:
+            self.setZoomFactor(self.zoomFactor() + 0.2))
+        self.zoomOut = QtGui.QShortcut("Ctrl+-", self, activated=lambda:
+            self.setZoomFactor(self.zoomFactor() - 0.2))
+        self.zoomOne = QtGui.QShortcut("Ctrl+0", self, activated=lambda:
+            self.setZoomFactor(1))
         self.settings().setAttribute(QtWebKit.QWebSettings.PluginsEnabled,
             True)
 
@@ -107,28 +142,6 @@ class Tab(QtWebKit.QWebView):
         self.load(url)
 
     amCurrent = lambda self: self.container.tabs.currentWidget() == self
-
-    def put(self, key, value):
-        "Persist an object somewhere under a given key"
-        settings.setValue(key, json.dumps(value))
-        settings.sync()
-
-    def get(self, key, default=None):
-        "Get the object under key in persistent storage, or the default value"
-        v = settings.value(key)
-        return json.loads(unicode(v.toString())) if v.isValid() else default
-
-    def bookmarkPage(self, v=None):
-        if v and v is not None:
-            self.bookmarks[unicode(self.url.text())] = \
-                unicode(self.windowTitle())
-        elif v is not None:
-            del (self.bookmarks[unicode(self.url.text())])
-        self.star.setMenu(QtGui.QMenu())
-        [self.star.menu().addAction(QtGui.QAction(title, self,
-            activated=lambda u=QtCore.QUrl(url):
-                self.load(u))) for url, title in self.bookmarks.items()]
-        self.put('bookmarks', self.bookmarks)
 
     def createWindow(self, windowType):
         return self.container.addTab()
