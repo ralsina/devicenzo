5f57be1edd2f444d984774e92f891e578b8af620
roberto.alsina <roberto.alsina@1bbba601-83ea-880f-26a2-52609c2bd284>
2011-03-06 12:19:07 +0000

    PEP8 fixes

--- devicenzo.py
+++ devicenzo.py
@@ -55,6 +55,7 @@ class MainWindow(QtGui.QMainWindow):
         [self.star.menu().addAction(QtGui.QAction(title, self, activated=lambda u=QtCore.QUrl(url): self.tabs.currentWidget().load(u))) for url, title in self.bookmarks.items()]
         self.put('bookmarks', self.bookmarks)
 
+
 class Tab(QtWebKit.QWebView):
     def __init__(self, url, container):
         self.container = container
